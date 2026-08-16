a03dc7be85b812db24f0cea98f0eeb119418f1f7
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2026-08-16 20:43:43 +0300

    refactor

---
+++ components/templates/ScreenContent.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+import { ExternalLink } from "../ExternalLink";
+import { Text, View } from "../Themed";
+
+import Colors from "@/constants/Colors";
+
+export default function ScreenContent({ path }: { path: string }) {
+  return (
+    <View>
+      <View style={styles.getStartedContainer}>
+        <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+          not native search
+        </Text>
+      </View>
+
+      <View style={styles.helpContainer}>
+        <ExternalLink
+          style={styles.helpLink}
+          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
+        >
+          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+            Tap here if your app doesn't automatically update after making
+            changes
+          </Text>
+        </ExternalLink>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: "center",
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: "center",
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: "center",
+  },
+});
